4ba362f328e5aefccac4d745fc7a20d6500d5f14
Anibal Solon <anibalsolon@gmail.com>
2021-04-12 12:07:53 -0400

    fix: change auth class

--- winrepo/settings.py
+++ winrepo/settings.py
@@ -106,6 +106,8 @@ LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+AUTH_USER_MODEL = 'profiles.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation'
