202c562f5ce2c804a469d3a2e4f45e638fc6b1c6
Anibal Solon <anibalsolon@gmail.com>
2021-04-16 19:52:20 -0400

    fix: variable replacement

--- tools/refresh_env.sh
+++ tools/refresh_env.sh
@@ -6,5 +6,5 @@ git pull
 ./tools/refresh_db.sh
 
 curl \
--H 'Authorization: Token ${PYTHONANYWHERE_TOKEN}' \
+-H "Authorization: Token ${PYTHONANYWHERE_TOKEN}" \
 https://www.pythonanywhere.com/api/v0/user/${PYTHONANYWHERE_USERNAME}/webapps/${PYTHONANYWHERE_DOMAIN}/reload/
