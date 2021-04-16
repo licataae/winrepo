fe83218fcaa2b15c1dbf742a2c06e983ac0c824c
Anibal Solon <anibalsolon@gmail.com>
2021-04-16 19:26:39 -0400

    enh: CI scripts

---
+++ tools/refresh_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ${HOME}/winrepo
+git pull
+
+./tools/refresh_env.sh
+
+curl \
+-H 'Authorization: Token ${PYTHONANYWHERE_TOKEN}' \
+https://www.pythonanywhere.com/api/v0/user/${PYTHONANYWHERE_USERNAME}/webapps/${PYTHONANYWHERE_DOMAIN}/reload/
