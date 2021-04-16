84431df51164cb34deb1076d6f7a990cb7c6c5df
Anibal Solon <anibalsolon@gmail.com>
2021-04-16 19:53:34 -0400

    fix: http method is post

--- tools/refresh_env.sh
+++ tools/refresh_env.sh
@@ -6,5 +6,6 @@ git pull
 ./tools/refresh_db.sh
 
 curl \
+-X POST \
 -H "Authorization: Token ${PYTHONANYWHERE_TOKEN}" \
 https://www.pythonanywhere.com/api/v0/user/${PYTHONANYWHERE_USERNAME}/webapps/${PYTHONANYWHERE_DOMAIN}/reload/
