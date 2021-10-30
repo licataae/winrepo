64e92b845ff15c840032072446501aae6e05808f
Anibal <anibalsolon@gmail.com>
2021-10-30 12:17:23 -0500

    enh: update refresh script

--- tools/refresh_env.sh
+++ tools/refresh_env.sh
@@ -6,6 +6,10 @@ BASEDIR=$(realpath $(dirname "$0")/..)
 cd ${BASEDIR}
 git pull
 
+python manage.py collectstatic
+python manage.py makemigrations
+python manage.py migrate
+
 # ./tools/refresh_db.sh
 
 curl \
